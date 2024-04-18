b662b11270a70cd5bbc294ad3acf90dbb20db885
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2024-04-18 17:13:15 -0400

    read ace library to adios2 file but doesn't support same variable name intergroup, Update CMakeLists.txt to include ADIOS2 and HighFive packages(still public linking)

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
-#export PapillonNDL_DIR=/users/hasanm4/sources/PCPEproject/install/papillon/lib64/cmake/PapillonNDL
+#gexport PapillonNDL_DIR=/users/hasanm4/sources/PCPEproject/install/papillon/lib64/cmake/PapillonNDL
 cmake -B build -S . \
-    -DPARXSREAD_USE_MPI=ON \
-    -DPapillonNDL_ROOT=/users/hasanm4/sources/PCPEproject/install/papillon
+    -DPARXSREAD_USE_MPI=OFF \
+    -DPapillonNDL_ROOT=/users/hasanm4/sources/PCPEproject/install/papillon \
+    -DHighFive_ROOT=/users/hasanm4/sources/PCPEproject/HighFive/installh5
--- read.cpp
+++ read.cpp
@@ -4,9 +4,15 @@
 
 #include <sstream>
 #include <iostream>
-#include <mpi.h>
+//#include <mpi.h>
+#include <adios2.h>
 #include <string>
+#include <vector>
+#include <sstream>
 #include "PapillonNDL/ace.hpp"
+
+void readxsdir(const std::string& filename);
+std::vector<std::string> splitString(const std::string& str);
 
 
 
@@ -20,12 +26,7 @@ int main(int argc, char* argv[])
     std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
     return 1;
   }
-/*
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  int mpi_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-*/
+
 
   // get the filename string from the first argument
   std::string filename = argv[1];
@@ -38,17 +39,72 @@ int main(int argc, char* argv[])
   acefile.save_hdf5(wfilename);
   std::cout << "ACE file is saved as HDF5." << std::endl;
 
-  // again convert the hdf5 file to binary
-  pndl::ACE acefile2(wfilename, pndl::ACE::Type::HDF5);
-  std::cout << "HDF5 file is read." << std::endl;
-  std::string wfilename2 = "test2.BIN";
-  acefile2.save_binary(wfilename2);
-  std::cout << "HDF5 file is saved as binary." << std::endl;
-/*
-  // finalize MPI
-  MPI_Finalize();
-*/
+
+
+  readxsdir("/lore/hasanm4/nuclearData/Lib80x/xsdir");
   
   return 0;
 }
 
+
+
+void readxsdir(const std::string &filename)
+{
+  std::ifstream file(filename);
+  if (!file.is_open())
+  {
+    std::cerr << "Could not open file: " << filename << std::endl;
+    return;
+  }
+
+  adios2::ADIOS adios;
+  adios2::IO io = adios.DeclareIO("writeIO");
+  adios2::Engine bpWriter = io.Open("ace.bp", adios2::Mode::Write);
+
+  adios2::Group group = io.InquireGroup('/');
+
+  bpWriter.BeginStep();
+
+  std::string line;
+  while (std::getline(file, line))
+  {
+    if (line.find("ptable") != std::string::npos)
+    {
+      continue; // skip this line
+    }
+    std::istringstream iss(line);
+    std::string zaid_text, col2, loc;
+    if (!(iss >> zaid_text >> col2 >> loc))
+    {
+      std::cerr << "Error in reading columns." << std::endl;
+      break; // error in reading columns
+    }
+    // std::cout << "Column 1: " << zaid_text << ", Column 3: " << loc << std::endl;
+    auto dirs = splitString(loc); // dirs = {Lib80x, molecule, zaid.temperature}
+
+    // read the ace file
+    std::string acefileloc = filename.substr(0, filename.find_last_of('/')) + "/" + loc; // root file name + / + loc
+    pndl::ACE acefile(acefileloc, pndl::ACE::Type::ASCII);
+    acefile.save_adios2(io, bpWriter, dirs[1], dirs[2], group);
+  }
+
+  bpWriter.EndStep();
+  bpWriter.Close();
+
+  file.close();
+}
+
+
+
+std::vector<std::string> splitString(const std::string& str) {
+    std::vector<std::string> tokens(3);
+    std::string token;
+    std::stringstream ss(str);
+    char delimiter = '/';
+
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
